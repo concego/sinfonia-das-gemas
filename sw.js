5347a334f6d7ee6852df3c65c120b03d6f7e8e2c
concego <euconcego@gmail.com>
2026-06-11 19:28:36 -0300

    Improve sw.js for PWA offline support

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
-// Service Worker básico
-self.addEventListener('install', e => e.waitUntil(caches.open('v1').then(c => c.addAll(['index.html']))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE_NAME = 'sinfonia-v2-beta';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './src/js/app.js',
+    './src/js/config.js',
+    './src/js/audio.js',
+    './src/js/ui.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
